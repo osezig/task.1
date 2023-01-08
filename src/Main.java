85672e5a26216e96f6d44f34d9815f71ba11fde2
sergey_iagfarov <dedaz27@mail.ru>
2023-01-08 19:02:33 +0500

    Задача № 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите число:");
+        String n = s.nextLine().trim();
+        char[] array = n.toCharArray();
+        int sum = 0;
+        for (char c : array) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException();
+            }
+            sum += Character.getNumericValue(c);
+                System.out.println("Сумма цифр в числе: " + sum);
+            }
+        }
+    }
